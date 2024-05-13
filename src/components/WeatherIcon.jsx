743b5a5dd4d45db9f6f0ae703b9ae37250c787eb
YeonhaPark <yonaprisca@gmail.com>
2024-05-13 17:48:16 +0900

    feat: add weather api

--- src/components/WeatherIcon.jsx
+++ src/components/WeatherIcon.jsx
@@ -3,14 +3,15 @@ import axios from "axios";
 const WeatherIcon = () => {
   const [latitude, setLatitude] = useState(0);
   const [longitutde, setLongitude] = useState(0);
-
+  const [weatherData, setWeatherData] = useState();
   const getWeather = async (lat, lon) => {
     try {
-      const data = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${
+      const { data } = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${lat}&lon=${lon}&appid=${
           import.meta.env.VITE_WEATHER_KEY
         }`
       );
+      setWeatherData(data);
       console.log({ data });
     } catch (e) {
       console.error(e);
@@ -19,10 +20,34 @@ const WeatherIcon = () => {
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       console.log(position);
+
       getWeather(position.coords.latitude, position.coords.longitude);
     });
   }, []);
-  return <div>WeatherIcon</div>;
+
+  if (!weatherData) {
+    return (
+      <div className="w-36 textgit credential-osxkeychain erase-xs">
+        Loading...
+      </div>
+    );
+  }
+  return (
+    <div className="text-xs">
+      {weatherData && (
+        <div className="flex items-center">
+          <img
+            className="w-12"
+            src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+          />
+          <div className="w-24">
+            <div>{weatherData.name}</div>
+            <div>{weatherData.main.temp.toFixed(1)}&#176;C</div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default WeatherIcon;
