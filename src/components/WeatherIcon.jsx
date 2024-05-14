e8d3f8cbe41eeb0668c6a14cee1e0b33eaa6ec2b
YeonhaPark <yonaprisca@gmail.com>
2024-05-14 10:38:46 +0900

    feat: 날씨 아이콘 추가

--- src/components/WeatherIcon.jsx
+++ src/components/WeatherIcon.jsx
@@ -1,9 +1,20 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import {
+  WiCloud,
+  WiCloudy,
+  WiDayCloudy,
+  WiFog,
+  WiRain,
+  WiSnowflakeCold,
+  WiStormShowers,
+} from "react-icons/wi";
+import { WiDaySunny } from "react-icons/wi";
 const WeatherIcon = () => {
   const [latitude, setLatitude] = useState(0);
   const [longitutde, setLongitude] = useState(0);
   const [weatherData, setWeatherData] = useState();
+
   const getWeather = async (lat, lon) => {
     try {
       const { data } = await axios.get(
@@ -12,34 +23,49 @@ const WeatherIcon = () => {
         }`
       );
       setWeatherData(data);
-      console.log({ data });
     } catch (e) {
       console.error(e);
     }
   };
+
+  const renderWeatherIcon = (icon) => {
+    switch (icon) {
+      case "01":
+        return <WiDaySunny size={32} />;
+      case "02":
+        return <WiDayCloudy size={32} />;
+      case "03":
+      case "04":
+        return <WiCloudy size={32} />;
+      case "09":
+      case "10":
+        return <WiRain size={32} />;
+      case "11":
+        return <WiStormShowers size={32} />;
+      case "13":
+        return <WiSnowflakeCold size={32} />;
+      case "50":
+        return <WiFog size={32} />;
+    }
+  };
+  useEffect(() => {
+    getWeather(latitude, longitutde);
+  }, [latitude, longitutde]);
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position);
-
-      getWeather(position.coords.latitude, position.coords.longitude);
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
     });
   }, []);
 
   if (!weatherData) {
-    return (
-      <div className="w-36 textgit credential-osxkeychain erase-xs">
-        Loading...
-      </div>
-    );
+    return <div className="w-[136px] text-sm">Loading...</div>;
   }
   return (
     <div className="text-xs">
       {weatherData && (
-        <div className="flex items-center">
-          <img
-            className="w-12"
-            src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
-          />
+        <div className="flex items-center gap-2">
+          {renderWeatherIcon(weatherData.weather[0].icon.substring(0, 2))}
           <div className="w-24">
             <div>{weatherData.name}</div>
             <div>{weatherData.main.temp.toFixed(1)}&#176;C</div>
